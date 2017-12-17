f1edc7eb048d7735eefbbaebf81f9ee0fe947aca
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2017-12-17 11:07:34 +0000

    indent in code.

--- pdf2tiff.sh
+++ pdf2tiff.sh
@@ -6,7 +6,7 @@ do
 	#Catch Process start time to calcuate process length
 	STARTTIME=$(date +"%s")
 	echo "Converting PDF to TIFF - $f..."
-  name=$(echo $f | cut -f 1 -d '.')
+	name=$(echo $f | cut -f 1 -d '.')
 	#Use GhostScript to convert pdf to tiff, resolution is 300dpi, file format is TIFFlzw
 	gs -dBATCH -dNOPAUSE -sDEVICE=tifflzw -r300 -dCOLORSCREEN=false -dNOINTERPOLATE -sOutputFile=$name.tiff $f
 	#Catch process end time
